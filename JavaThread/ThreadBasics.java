029865644b8e0998a125ff1295f3f4efeea247fe
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2023-01-23 20:31:21 +0530

    Thread

--- JavaThread/ThreadBasics.java
+++ JavaThread/ThreadBasics.java
@@ -1,6 +1,38 @@
 package JavaThread;
 
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadBasics {
+
+    public static class Car implements Runnable{
+
+        String name;
+        Car(String name){
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("[ "+this.name + " is running ]");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Car tata = new Car("Nexon");
+        Car mahindra = new Car("Scorpio");
+
+        Thread thread1 = new Thread(tata);
+        Thread thread2 = new Thread(mahindra);
+
+        //Executor
+        ScheduledExecutorService service = new ScheduledThreadPoolExecutor(5);
+        service.scheduleAtFixedRate(tata, 5000, 10000, TimeUnit.MILLISECONDS);
+
+
+    }
 
 
 }
