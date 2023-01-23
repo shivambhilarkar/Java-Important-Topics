8c49f7df1016994296d4d0f1ad1180862d60b8be
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2023-01-23 22:51:30 +0530

    Regular Expresions in Java

---
+++ JavaRegularExpressions/JavaRegularExp.java
@@ -0,0 +1,31 @@
+package JavaRegularExpressions;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JavaRegularExp {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //pattern 4 letter @ and 3 digit
+        //Pattern pattern = Pattern.compile("^\\w{4}@\\d{3}$");
+
+        //pattern is 10 digit no
+        //Pattern pattern = Pattern.compile("^\\d{10}$");
+
+        //username more than 5 length long
+        Pattern pattern =  Pattern.compile("^\\w{5,}$");
+
+        System.out.print("[ Enter Input ] : ");
+        String input = scan.next();
+
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.find()){
+            System.out.println("[ Pattern is matched ]");
+        }else{
+            System.out.println("[ Pattern didn't matched ]");
+        }
+    }
+}
---
+++ JavaRegularExpressions/ReadMe.md
@@ -0,0 +1,17 @@
+### Important Regular Expression
+````
+-  [ abc ]    = A Single character of : a, b or c
+-  [a - z]    = A single character in a range : a to z
+-  a | b      = Match either a or b
+-   \d        = A digit : [ 0 - 9 ]
+-   \w        = A word character : [a-zA-Z_0-9]
+-    .        = Any Single Character
+-   a{3}      = Exactly 3 of a :aaa
+-   a{3,}     = 3 or more of a
+-   a{2,4}    = between 2 or 4 of a : aa, aaa or aaaa
+-    ^        = the beginning of a line
+-    $        = The end of a lin
+-    *        = Matches zero or more occurrences.
+-    +        = Matches one or more occurrences.
+-    ?        = Matches zero or one occurrences.
+````
